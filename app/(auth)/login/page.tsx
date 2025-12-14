72154c086b5dc9f3172aea5275d041109ed920c1
SwayamWakodikar <swayam.w06@gmail.com>
2025-12-14 23:31:35 +0530

    modified signup form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,10 +3,11 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 import LoginPage from '@/components/LoginPage'
+import SignupFormDemo from '@/components/signup-form-demo'
 const page = () => {
   return (
     <div className=''>
-        <LoginPage/>
+        <SignupFormDemo/>
     </div>
   )
 }
